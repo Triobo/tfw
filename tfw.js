d0b23c10931e78a7bf3918b728dae57d0b7c03e0
melanger <melanger@users.noreply.github.com>
2018-02-02 10:42:23 +0100

    Revert "removed HTMLElement.add"
    This reverts commit 22f5b9755912c5fbb3fc72efe0f808abc8e32cc9.

--- tfw.js
+++ tfw.js
@@ -18,6 +18,10 @@ function $(id){//eslint-disable-line no-implicit-globals
   return x;
 }
 
+HTMLElement.prototype.add = function(x){
+  this.appendChild(x);
+};
+
 function initProperty (object, name, defaultValue) {
   if (!(name in object)) object[name] = defaultValue;
 };
